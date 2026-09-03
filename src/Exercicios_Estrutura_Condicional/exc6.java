a1f55a80d977a7144b8577f3e973a77f8f988aab
Soares90 <nicolassoaresmatos65@gmail.com>
2026-09-03 18:16:39 -0300

    exc6

--- src/Exercicios_Estrutura_Condicional/exc6.java
+++ src/Exercicios_Estrutura_Condicional/exc6.java
@@ -1,6 +1,35 @@
 package Exercicios_Estrutura_Condicional;
-
+import java.util.Scanner;
 public class exc6 {
+
+    public static void main(String[]args){
+
+        Scanner leia = new Scanner(System.in);
+
+        double numero;
+
+        System.out.println("Digite um número:");
+        numero = leia.nextDouble();
+
+        if (numero >=0 && numero <=25){
+            System.out.println("Intervalo [0,25]");
+        }
+        else if (numero >25 && numero <=50){
+            System.out.println("Intervalo [25,50]");
+        }
+        else if (numero >50 && numero <=75){
+            System.out.println("Intervalo [50,75]");
+        }
+        else if (numero >75 && numero <=100){
+            System.out.println("Intervalo [75,100]");
+        }
+        else{
+            System.out.println("Fora do intervalo");
+        }
+
+    }
+
+
 
 
 }
